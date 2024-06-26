3399548cc4aafe6e8474cc1e688c5684d3ed73b4
Kira <31421550+kirawc@users.noreply.github.com>
2024-06-25 22:20:07 -0400

    Updates

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
 $(document).ready(function(){
 
-  $("#cvbutton").click(function(){showCV()})
   $("#pubbutton").click(function(){showpubs()})
+  $("#projbutton").click(function(){showproj()})
   $("#homebutton").click(function(){gohome()})
-  $("#links").click(function(){showlinks()})
 
 
 });
 
-function showCV(){
-  $(".block4").show(); // show cv iframe
-  $(".block2").hide(); // hide bio
-  $(".block3").hide(); // hide publications list
-  $(".block5").hide(); // hide links
-
-}
-
 function showpubs(){
   $(".block3").show(); // show publication list
   $(".block2").hide(); // hide bio
-  $(".block4").hide(); // hide cv iframe
-  $(".block5").hide(); // hide links
+  $(".block4").hide(); // hide projects
 }
+
+function showproj(){
+  $(".block4").show(); // show projects
+  $(".block2").hide(); // hide bio
+  $(".block3").hide(); // show publication list
+}
+
 
 function gohome(){
   $(".block2").show(); // show bio
   $(".block3").hide(); // hide publication list
-  $(".block4").hide(); // hide cv frame
-  $(".block5").hide(); // hide links
-
-}
-
-function showlinks(){
-  $(".block5").show(); // hide links
-  $(".block2").hide(); // show bio
-  $(".block3").hide(); // hide publication list
-  $(".block4").hide(); // hide cv frame
+  $(".block4").hide(); // hide projects
 
 }
